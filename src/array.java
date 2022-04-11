0c6839785e7d08572fd59a720de8d53c1c0b8d0c
Sam Smith <sam@example.com>
2022-04-11 19:01:34 +0530

    first commit

---
+++ src/array.java
@@ -0,0 +1,19 @@
+
+
+
+public class array {
+
+	public static void main(String[] args) {
+		
+		String x = "hjsdhjhdj";
+ System.out.print(x);
+		
+		
+		
+		
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
